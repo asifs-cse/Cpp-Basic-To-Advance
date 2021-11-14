d8a94c47b7edcbd4b37d2ee7b65c7263a97a1ac8
asifs-cse <asifs.cse@gmail.com>
2021-11-14 23:17:12 +0530

    keyword datatype and veriable

---
+++ kyewordVariableDataType.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int a=20;
+    float b=20.456789;
+    double c = 20.456789;
+    char d = 'A';
+
+    cout<<a<<endl<<b<<endl<<c<<endl<<d;
+    getch();
+}
