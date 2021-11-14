76f20cf58f72033593358c9dc6d7d40c7cb2d307
asifs-cse <asifs.cse@gmail.com>
2021-11-14 23:46:46 +0530

    using formatting

---
+++ Formatting.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<<"Enter first value: ";
+    cin>>num1;
+    cout<<"Enter second value: ";
+    cin>>num2;
+    
+    float div;
+    div = (float) num1/num2;
+    cout<<div;
+    getch();
+}
