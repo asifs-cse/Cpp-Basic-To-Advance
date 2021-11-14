766f0bbabe9c638d422490103b3fd8e7355e80b0
asifs-cse <asifs.cse@gmail.com>
2021-11-14 23:22:14 +0530

    string datatype

---
+++ stringDataType.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    char name = 'Asif Shahriar'; //char print only one character 
+    cout<<name<<endl; // name print only r
+
+    char fullname[]= "Asif Shahriar"; //declare a char array and store many data
+    cout<<fullname;
+    getch();
+}
