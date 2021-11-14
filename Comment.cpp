61bf5974e1ddaed14cfc6a25d7303d3ff1a8a077
asifs-cse <asifs.cse@gmail.com>
2021-11-14 23:06:36 +0530

    Comment

---
+++ Comment.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    //enter your name
+    cout<<"Hello Asif";
+    //enter your roll
+    cout<<"\nYour Roll is: 20MH1A0504";
+    /*enter your country
+    and your branch*/
+    cout<<"I am from bangladesh"<<endl;
+    cout<<"My branch is CSE";
+
+    getch();
+}
