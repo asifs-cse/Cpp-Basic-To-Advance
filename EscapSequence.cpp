48e5dc8c48c957c80c13ce5bea370c8d386f3231
asifs-cse <asifs.cse@gmail.com>
2021-11-14 23:02:29 +0530

    escap sequence

---
+++ EscapSequence.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+    cout<<"\"Asif Shahriar\""<<endl;
+    cout<<"\v20MH1A050\n";
+    cout<<"\tCSE"<<endl;
+    getch();
+}
