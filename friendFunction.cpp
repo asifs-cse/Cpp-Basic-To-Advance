3f12cef30890640aab74e0bd94f1832ae9d152a2
asifs-cse <asifs.cse@gmail.com>
2021-11-24 00:14:27 +0530

    not using firend keyword

--- friendFunction.cpp
+++ friendFunction.cpp
@@ -6,16 +6,25 @@ class rectangle{
     int height;
     int width;
 public:
-    void set(int a, int b){
-        height = a; width =b;
-    }
+    void set(int a, int b){height = a; width =b;}
     int area(){
         return height*width;
+    }
+};
+class cost{
+    int costRate;
+public:
+    void setCostValue(int a){costRate = a;}
+    int totalCost(rectangle A){
+        return costRate*A.area();
     }
 };
 int main(){
     rectangle r;
     r.set(5,6);
     cout<<r.area()<<endl;
+    cost c;
+    c.setCostValue(90);
+    cout<<c.totalCost(r)<<endl;
     return 0;
 }
