aebe782793ac6a6fd8367abee814fd92becd7040
asifs-cse <asifs.cse@gmail.com>
2021-11-24 07:56:52 +0530

    access specifieres

---
+++ AccessSpecifieres.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class A{
+    protected:
+    int x;
+    public:
+    A(int p){
+        x=p;
+    }
+};
+class B:public A{
+    private:
+    int y;
+    public:
+    B(int p, int q):A(p){
+        y=p;
+    }
+    void show(){
+        cout<<"X = "<<x<<endl;
+        cout<<"Y = "<<y<<endl;
+    }
+};
+
+int main(){
+
+    B obj(10,20);
+    obj.show();
+    getch();
+    return 0;
+}
