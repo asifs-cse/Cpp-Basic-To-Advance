f7b19dd0487184791768ed2c37726d13d9ae81fa
asifs-cse <asifs.cse@gmail.com>
2021-11-24 07:19:49 +0530

    using constractor

---
+++ Constractor.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Distance{
+    private:
+    int feet;
+    int inches;
+    public:
+    Distance(){};
+    Distance(int f, int i){
+        feet=f;
+        inches =i;
+    }
+    void get_distance(){
+        cout<<"Distance is feet = "<<feet<<", inches = "<<inches<<endl;
+    }
+    void add(Distance &d1, Distance &d2){
+        feet = d1.feet+d2.feet;
+        inches = d1.inches+d2.inches;
+        feet = feet+(inches/12);
+        inches = inches%12;
+    }
+    ~Distance(){
+        cout<<"Distance object destroyed"<<endl;
+    }
+};
+
+int main(){
+    int f1,in1, f2,in2;
+    cout<<"Enter feet: ";
+    cin>>f1;
+    cout<<"Enter inches: ";
+    cin>>in1;
+    cout<<"Enter feet: ";
+    cin>>f2;
+    cout<<"Enter inches: ";
+    cin>>in2;
+
+    Distance d1(f1,in1);
+    Distance d2(f2,in2);
+    Distance d3;
+    d3.add(d1, d2);
+    d3.get_distance();
+
+
+    getch();
+    return 0;
+}
