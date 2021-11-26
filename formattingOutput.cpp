91bdf19e9ae83c52edcab1199012a9f56593f85c
asifs-cse <asifs.cse@gmail.com>
2021-11-26 23:22:01 +0530

    using showpoint keyword

---
+++ formattingOutput.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+
+    double num1,num2;
+    cout<<"Enter first value: ";
+    cin>>num1;
+    cout<<"Enter second value: ";
+    cin>>num2;
+
+    cout<<showpoint;
+    
+    cout<<"SUM = "<<num1+num2<<endl;
+    cout<<"SUB = "<<num1-num2<<endl;
+    cout<<"MUL = "<<num1*num2<<endl;
+    cout<<"DIV = "<<num1/num2;
+    getch();
+
+    return 0;
+}
