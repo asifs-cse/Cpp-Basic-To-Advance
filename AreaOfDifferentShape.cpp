cd9ff8ad09a3a154a3bf0c38f51c08246c3d2551
asifs-cse <asifs.cse@gmail.com>
2021-11-27 00:58:49 +0530

    area of triangle

---
+++ AreaOfDifferentShape.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+    double base, height, width;
+    cout<<"Enter Height, Width & Base value: ";
+    cin>>base>>height>>width;
+
+    cout<<"Triangle Area is: "<<1.5*base*height;
+    
+    return 0;
+}
