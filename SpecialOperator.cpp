a9a8d4c7a469dc2da499a101e1e2f607da60d838
asifs-cse <asifs.cse@gmail.com>
2021-11-30 00:02:15 +0530

    special operator

---
+++ SpecialOperator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int a;
+    float f;
+    double d;
+    char ch;
+    char name[20];
+
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(f)<<endl;
+    cout<<sizeof(d)<<endl;
+    cout<<char(ch)<<endl;
+    cout<<sizeof(name);
+    return 0;
+}
