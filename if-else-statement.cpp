5d2b739b6817579e4dbb6477c7918927b119ce15
asifs-cse <asifs.cse@gmail.com>
2021-11-30 00:20:46 +0530

    if else statement

---
+++ if-else-statement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int a=5;
+    if (a==5)
+    {
+        cout<<"A value is 5";
+    }else{
+        cout<<"A value not equal is 5";
+    }
+    
+    return 0;
+}
