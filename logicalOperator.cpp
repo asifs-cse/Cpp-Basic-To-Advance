e52d65aa7820d66fbf0bc3abac8da9d38a00573e
asifs-cse <asifs.cse@gmail.com>
2021-12-01 20:29:11 +0530

    logical operator

---
+++ logicalOperator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<<"Enter two value: ";
+    cin>>num1>>num2;
+
+    if(num1==num2 && num1==num2){
+        cout<<"Equal";
+    }else if(num1+num2 > num1 || num1+num2>num2){
+        cout<<"Equal or not";
+    }
+    return 0;
+}
