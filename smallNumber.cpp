a83670017b7fd40ae9a3bc2e18487345a8546349
asifs-cse <asifs.cse@gmail.com>
2021-12-01 23:18:53 +0530

    smallest number of three

---
+++ smallNumber.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"Enter b: ";
+    cin>>b;
+    cout<<"Ente c: ";
+    cin>>c;
+
+    if (a<=b && a<=c)
+    {
+        cout<<"Smallest number is: "<<a;
+    }else if(b<=a && b<=c){
+        cout<<"Smallest number is: "<<b;
+    }else{
+        cout<<"Smallest number is: "<<c;
+    }
+    
+    return 0;
+}
