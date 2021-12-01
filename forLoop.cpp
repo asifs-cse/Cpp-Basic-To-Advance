5101d59252e86279467fa217d110ce4871989850
asifs-cse <asifs.cse@gmail.com>
2021-12-02 00:17:52 +0530

    for loop

---
+++ forLoop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int start, end;
+    cout<<"Ente start number: ";
+    cin>>start;
+    cout<<"Enter end number: ";
+    cin>>end;
+    for(int i=start; i<=end; i++){
+        cout<<i<<endl;
+    }
+    return 0;
+}
