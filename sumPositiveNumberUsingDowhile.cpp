bc4e2a4acb5ae0909f5877951e866c6798a05d24
asifs-cse <asifs.cse@gmail.com>
2021-12-02 11:38:57 +0530

    sum positive number using do while

--- sumPositiveNumberUsingDowhile.cpp
+++ sumPositiveNumberUsingDowhile.cpp
@@ -1,1 +1,18 @@
-#include<ios
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int number, sum=0;
+    cout<<"Enter a number: ";
+    cin>>number;
+
+    do{
+        sum+=number;
+        cout<<"Enter a number: ";
+        cin>>number;
+    }while(number>=0);
+
+    cout<<"Sum of positive number: "<<sum;
+    return 0;
+}
