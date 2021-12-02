dd33be96c640b6d00a1bf7e336e6e448fdb4b109
asifs-cse <asifs.cse@gmail.com>
2021-12-02 11:03:53 +0530

    while loop

---
+++ whileLoop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int s,e;
+    cout<<"Ente start value: ";
+    cin>>s;
+    cout<<"Ente end value: ";
+    cin>>e;
+    
+    while (s<=e)
+    {
+        cout<<s<<endl;
+        s++;
+    }
+    
+    return 0;
+}
