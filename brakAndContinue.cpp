7c9a87d5193898dbe5b0252e456e37fcdfcf797b
asifs-cse <asifs.cse@gmail.com>
2021-12-04 12:58:11 +0530

    using break keyword

---
+++ brakAndContinue.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    for (int i = 0; i < 100; i++)
+    {   
+        if(i==10){break;}
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
