403f3830b47b2d1839f002f6c98781ae4c9de910
asifs-cse <asifs.cse@gmail.com>
2021-12-09 00:40:12 +0530

    input first array

---
+++ singleAarraySum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int a[10],b[10],c[10];
+    cout<<"Enter first array values: "<<endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<"Enter "<<i<<" value: ";
+        cin>>a[i];
+    }
+    
+    
+    
+
+    return 0;
+}
