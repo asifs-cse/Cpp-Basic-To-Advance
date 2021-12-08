c5f90b6a5ea58c6546ded3feac1a1e38e542cadd
asifs-cse <asifs.cse@gmail.com>
2021-12-09 00:53:59 +0530

    add a double array value

---
+++ doubleArraySum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int a[2][2], b[2][2], c[2][2];
+    cout<<"Enter first array values: "<<endl;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j< 2; j++)
+        {
+            cout<<"Enter "<<i<<j<<" Value: ";
+            cin>>a[2][2];
+        }
+    }
+    
+    
+
+    return 0;
+}
