279acbe7e1cc6b815de425c74210ff67e7eecd62
asifs-cse <asifs.cse@gmail.com>
2021-12-15 22:32:35 +0530

    print 2d array

---
+++ twoDArray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void print2DArray(int testArray[2][2]);
+
+int main(){
+    int test[2][2];
+    test[0][0]=5;
+    test[0][1]=6;
+    test[1][0]=7;
+    test[1][1]=9;
+
+    print2DArray(test);
+
+    return 0;
+}
+void print2DArray(int arr[2][2]){
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<"\n";
+        
+    }
+    
+}
