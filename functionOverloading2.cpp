b856bedee0fe5985eb15034907238b1b2cd82ee4
asifs-cse <asifs.cse@gmail.com>
2021-12-18 18:24:41 +0530

    constration

---
+++ functionOverloading2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class summation{
+    int n;
+    summation(int x){
+        n=x;
+        cout<<n;
+        cout<<"Hello";
+    }
+};
+
+int main(){
+    int num =10;
+    summation s();
+
+    return 0;
+}
