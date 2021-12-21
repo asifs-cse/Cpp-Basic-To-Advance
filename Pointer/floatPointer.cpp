a9513a7022c7d6d39147cd42c20e300e1c890566
asifs-cse <asifs.cse@gmail.com>
2021-12-21 23:59:58 +0530

    store float  value memory address

---
+++ Pointer/floatPointer.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    float f=10.5;
+    float* fpr = &f;
+    cout<<fpr<<endl;
+    cout<<*fpr<<endl;
+    cout<<&f<<endl;
+    *fpr = 45.56;
+    cout<<f;
+    return 0;
+}
