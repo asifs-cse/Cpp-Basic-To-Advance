c1534db83d499f79134726a5a6779b545d07afaf
asifs-cse <asifs.cse@gmail.com>
2021-12-22 00:15:46 +0530

    pass address by using pointer

--- Pointer/pfunction.cpp
+++ Pointer/pfunction.cpp
@@ -2,13 +2,20 @@
 #include<conio.h>
 using namespace std;
 int testValue(int x);
+int testPValue(int* y);
 int main(){
     int num =20;
-    cout<<testValue(num);
+    cout<<testValue(num)<<endl;
+    cout<<testPValue(&num)<<endl;
     return 0;
 }
 int testValue(int n){
     cout<<n<<endl;
     n =90;
     return n;
+}
+int testPValue(int* add){
+    cout<<*add<<endl;
+    *add =56;
+    return *add;
 }
