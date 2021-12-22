6c55c5f4645b8b547f2cd30bbef2ceaf4efb10d4
asifs-cse <asifs.cse@gmail.com>
2021-12-22 22:15:45 +0530

    size of struct

---
+++ Pointer/structuceSize.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+struct mStruct{
+    int i;
+    char c;
+    double d;
+};
+int main(){
+    cout<<sizeof(struct mStruct);
+    return 0;
+}
