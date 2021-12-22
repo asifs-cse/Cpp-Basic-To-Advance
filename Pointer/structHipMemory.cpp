8cdba65f4c13aa8647855250ef5248d7f61011eb
asifs-cse <asifs.cse@gmail.com>
2021-12-23 00:06:35 +0530

    dynamic allocation

--- Pointer/structHipMemory.cpp
+++ Pointer/structHipMemory.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
+struct mStruct{
+    int age;
+    double height;
+};
 int main(){
-    
+    struct mStruct* ps;
+    ps = (struct mStruct*) malloc(sizeof(struct mStruct)); //memory allocation in hip memory
+    ps->age =30;
+    ps->height =4.7;
+    cout<<ps->age<<endl<<ps->height;
+
     return 0;
 }
