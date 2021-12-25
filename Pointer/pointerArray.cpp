8cfdbc3c615e03869388778cf774df03ea4216f6
asifs-cse <asifs.cse@gmail.com>
2021-12-25 21:27:08 +0530

    show array first elliment using pointer

--- Pointer/pointerArray.cpp
+++ Pointer/pointerArray.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
     int arr[3] ={1,2,3};
-    cout<<arr; //when we print array variable name then comiler show array first index address
+    cout<<arr<<endl; //when we print array variable name then comiler show array first index address
+    cout<<*arr+0;
     return 0;
 }
