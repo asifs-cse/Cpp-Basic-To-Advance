a2d6ae802087f194d4e6924efb1e95f69f783416
asifs-cse <asifs.cse@gmail.com>
2021-12-25 21:56:06 +0530

    array insert data using pointer and index

--- Pointer/dynamicArrayPointer.cpp
+++ Pointer/dynamicArrayPointer.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 int main(){
     int arr[5] = {4,8,9};
-    int *arr2 = (int*) malloc(12);
+    int *arr2 = (int*) malloc(3*sizeof(int));
     *arr2 ={3};
-    cout<<sizeof(arr)<<endl<<sizeof(arr2);
+
+    arr2[0] =1;
+    arr2[1] =2;
+    arr2[2] =3;
+    *(arr2+2) = 4;
+
+    cout<<sizeof(arr)<<endl<<sizeof(arr2)<<endl;
+    cout<<*arr2<<endl;
+    cout<<*arr2+2;
     return 0;
 }
