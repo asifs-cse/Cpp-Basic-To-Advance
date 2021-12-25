358bd4b17d69d4610adb936dafabf22bce78adef
asifs-cse <asifs.cse@gmail.com>
2021-12-25 21:19:51 +0530

    show array first address

---
+++ Pointer/pointerArray.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int arr[3] ={1,2,3};
+    cout<<arr; //when we print array variable name then comiler show array first index address
+    return 0;
+}
