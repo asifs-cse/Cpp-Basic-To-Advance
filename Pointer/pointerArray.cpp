2df95667ba5700867d9d98c2498c092ea672dcf4
asifs-cse <asifs.cse@gmail.com>
2021-12-25 21:28:17 +0530

    show array first elliment using index number

--- Pointer/pointerArray.cpp
+++ Pointer/pointerArray.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
     int arr[3] ={1,2,3};
     cout<<arr<<endl; //when we print array variable name then comiler show array first index address
-    cout<<*arr+0;
+    cout<<*arr+0<<endl; //show array first element using pointer
+    cout<<arr[0]<<endl; //show array first element using index number
     return 0;
 }
