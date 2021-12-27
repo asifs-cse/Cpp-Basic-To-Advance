e4cf022525b11a9aabfacd491209f9f0ad12801a
asifs-cse <asifs.cse@gmail.com>
2021-12-27 23:30:36 +0530

    input process time

--- roundRobinAlgorith.cpp
+++ roundRobinAlgorith.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main(){
     int n,i,qt,count=0, temp, sq=0, bt[10], wt[10], tat[10], rem_bt[10]; //int varibale
     float awt=0, atat=0; //float variable
+    cout<<"Enter number of process: ";
+    cin>>n; //input process number
     return 0;
 }
