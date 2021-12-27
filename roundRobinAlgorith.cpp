bea2f54735218f13cf51e247ead31c08286016c3
asifs-cse <asifs.cse@gmail.com>
2021-12-27 23:29:03 +0530

    roundrobin float variable

--- roundRobinAlgorith.cpp
+++ roundRobinAlgorith.cpp
@@ -2,6 +2,7 @@
 #include<conio.h>
 using namespace std;
 int main(){
-    int n,i,qt,count=0, temp, sq=0, bt[10], wt[10], tat[10], rem_bt[10]; //input varibale
+    int n,i,qt,count=0, temp, sq=0, bt[10], wt[10], tat[10], rem_bt[10]; //int varibale
+    float awt=0, atat=0; //float variable
     return 0;
 }
