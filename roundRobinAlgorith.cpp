2d7faf3fc408d9e980cb99c90ff0eaaed20d97ff
asifs-cse <asifs.cse@gmail.com>
2021-12-28 23:24:09 +0530

    round robin algorithm

--- roundRobinAlgorith.cpp
+++ roundRobinAlgorith.cpp
@@ -8,10 +8,52 @@ int main(){
     cin>>n; //input process number
     for (i = 0; i < n; i++)
     {
-        cout<<"Enter burst time of process "<<i<<" :";
+        cout<<"Enter burst time of process "<<i+1<<" :";
         cin>>bt[i]; //input burst time
+        rem_bt[i] = bt[i];
     }
-    
+    cout<<"Enter quantum time: ";
+    cin>>qt;
+    while(1){
+        for (i = 0;count=0; i++)
+        {
+            temp=qt;
+            if (rem_bt[i]==0)
+            {
+                count++;
+                continue;
+            }
+            if (rem_bt[i]>qt)
+            {
+                rem_bt[i] = rem_bt[i]-qt;
+            }else{
+                if (rem_bt[i]>=0)
+                {
+                    temp =rem_bt[i];
+                    rem_bt[i]=0;
+                }
+                sq= sq+temp;
+                tat[i] =sq;
+            } 
+        }
+        if (n==count)
+        {
+            break;
+        }   
+    }
+    //show output
+    cout<<"\nProcess \tburst time \tturnaround time \twaiting time\n"; 
+    for (i = 0; i < n; i++)
+    {
+        wt[i] = tat[i]-bt[i];
+        awt=awt+wt[i];
+        atat=atat+tat[i];
+        cout<<"\ni+1\t\t"<<bt[i]<<"\t\t"<<tat[i]<<"\t\t"<<wt[i];
+    }
+    awt=awt;
+    atat=atat;
+    cout<<"Average waiting time = "<<awt;
+    cout<<"Average turnaround time ="<<atat;
     
     
     return 0;
