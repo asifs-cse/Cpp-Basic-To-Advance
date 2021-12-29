230e956b7f4dc208259443c10495e02a3554bd57
asifs-cse <asifs.cse@gmail.com>
2021-12-29 21:56:43 +0530

    pass int value in template function

--- Pointer/templage.cpp
+++ Pointer/templage.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
+
+template <class T>
+T add(T a, T b){
+    return a+b;
+}
 int main(){
-    cout<<"Templage here!";
+    int a=5,b=6;
+    cout<<"Intiger sum: "<<add(a,b);
     return 0;
 }
