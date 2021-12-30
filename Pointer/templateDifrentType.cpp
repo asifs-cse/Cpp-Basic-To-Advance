3ef94dbbc50ad838f487690dacff3ca6eabffd5b
asifs-cse <asifs.cse@gmail.com>
2021-12-30 22:34:47 +0530

    send integer value

--- Pointer/templateDifrentType.cpp
+++ Pointer/templateDifrentType.cpp
@@ -6,8 +6,8 @@ void print(T1 a, T2 b){
     cout<<a<<" "<<b<<endl; 
 }
 int main(){
-    print(1,1);
+    print(1,1); //send integer value
     print(1,1.5);
-    print(1,"Asif");
+    print(1,"Asif"); 
     return 0;
 }
