7b0112e973b8ed55fc0fe115dcfec802efbb6347
asifs-cse <asifs.cse@gmail.com>
2021-12-30 22:39:52 +0530

    any type of argument value accept

--- Pointer/templateDifrentType.cpp
+++ Pointer/templateDifrentType.cpp
@@ -6,7 +6,7 @@ void print(T1 a, T2 b){ //accept any type of value
     cout<<a<<" "<<b<<endl; //print value here
 }
 int main(){
-    print(1,1); //send integer value
+    print(1,1); //send integer value //
     print(1,1.5); //send integer and float value //
     print(1,"Asif"); //send integer and string value 
     return 0;
