1d2aaf0543767c32562225ab757de9d012bfdc7b
asifs-cse <asifs.cse@gmail.com>
2022-01-01 21:21:57 +0530

    template call

--- Pointer/templage.cpp
+++ Pointer/templage.cpp
@@ -7,11 +7,11 @@ T add(T a, T b){ //template defination
     return a+b; //return value
 }
 int main(){
-    int a=5,b=6;
+    int a=5,b=6; 
     float c=3.4, d=9.5;
     string e="Asif ", f="Shahriar";
-    cout<<"Intiger sum: "<<add(a,b)<<endl;
-    cout<<"Float sum: "<<add(c,d)<<endl;
-    cout<<"String sum: "<<add(e,f)<<endl;
+    cout<<"Intiger sum: "<<add(a,b)<<endl; //function call
+    cout<<"Float sum: "<<add(c,d)<<endl;  //function call
+    cout<<"String sum: "<<add(e,f)<<endl; //function call
     return 0;
 }
