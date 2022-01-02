c19c9c8ed537b84c5905d24c5bba6bb12f616bd1
asifs-cse <asifs.cse@gmail.com>
2022-01-02 22:49:39 +0530

    string variable

--- Pointer/templage.cpp
+++ Pointer/templage.cpp
@@ -9,7 +9,7 @@ T add(T a, T b){ //template defination
 int main(){
     int a=5,b=6; 
     float c=3.4, d=9.5;
-    string e="Asif ", f="Shahriar";
+    string e="Asif ", f="Shahriar";//string variable defination
     cout<<"Intiger sum: "<<add(a,b)<<endl; //function call
     cout<<"Float sum: "<<add(c,d)<<endl;  //function call
     cout<<"String sum: "<<add(e,f)<<endl; //function call
