9aef1099d27840bf1fbb2acd0d9eb2127469e9ea
asifs-cse <asifs.cse@gmail.com>
2022-01-04 22:10:41 +0530

    veriable declear

--- evenOddWithoutModulus3.cpp
+++ evenOddWithoutModulus3.cpp
@@ -6,7 +6,7 @@ bool isEven(int n){
     return (!(n&1)); //return factorial number
 }
 int main(){
-    int n =6;
+    int n =6; //veriable declear and define value
     isEven(n) ? cout<<"Even" : cout<<"Odd";  //function call
     return 0;
 }
