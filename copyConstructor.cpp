c4ade79356bd63e5ec5af81ab3c0bc1d1350c671
asifs-cse <asifs.cse@gmail.com>
2022-01-05 23:19:24 +0530

    copy constructor

---
+++ copyConstructor.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Wall{
+    private:
+    double length;
+    double height;
+
+    public:
+    Wall(double len, double hgt){
+        length = len;
+        height = len;
+    }
+    Wall(Wall &obj){
+        length = obj.length;
+        height = obj.height;
+    }
+    double claculateArea(){
+        return height*length;
+    }
+};
+int main(){
+    Wall wall(10.5, 8.6);
+    cout<<"Area of Wall : "<<wall.claculateArea()<<endl;
+    return 0;
+}
