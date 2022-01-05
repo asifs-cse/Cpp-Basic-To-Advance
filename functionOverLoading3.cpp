0a00ceaf0a1a327ed9569e8fbf49786e19dc4626
asifs-cse <asifs.cse@gmail.com>
2022-01-05 21:40:02 +0530

    function overloading

--- functionOverLoading3.cpp
+++ functionOverLoading3.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
+
+float absolute(float var){
+    if (var<0.0)
+    {
+        var = -var;
+    }
+    return var;   
+}
+int absolute(int var){
+    if (var< 0)
+    {
+        var = -var;
+    }
+    return var;
+    
+}
 int main(){
-    cout<<"function overloading";
+    cout<<"function overloading"<<endl;
+    cout<<"Absolute value of -5: "<<absolute(-5)<<endl;
+    cout<<"Absolute value of 5.5: "<<absolute(-5.5f)<<endl;
     return 0;
 }
