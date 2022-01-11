6c79c644c9923f999c54e76fecefd50f6ffbde00
asifs-cse <asifs.cse@gmail.com>
2022-01-11 22:29:18 +0530

    private variable

--- defultConstructor.cpp
+++ defultConstructor.cpp
@@ -2,7 +2,7 @@
 #include<conio.h>
 using namespace std;
 class Wall{
-    private:
+    private: //private variable
     double length;
     public:
     Wall(){ //defult consturctor
@@ -13,6 +13,5 @@ class Wall{
 };
 int main(){
     Wall wall1; //call constructor
-    return 0;
     return 0;
 }
