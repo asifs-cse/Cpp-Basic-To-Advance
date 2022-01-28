b4b1a5a8349c6d6dca55ef4a4b54cbc44d9dc56e
asifs-cse <asifs.cse@gmail.com>
2022-01-28 20:58:29 +0530

    template function overloading

---
+++ TemplateFunctionOverloading.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<class T>
+T add(T a){
+    return a;
+}
+template<class T1, class T2>
+T1 add(T1 a, T2 b){
+    return a+b;
+}
+void add(int a, int b){
+    cout<<"Addition two int: "<<a+b;
+}
+int main(){
+    cout<<"Addition int and float: "<<add(9.5, 10)<<endl;
+    cout<<"Return same value: "<<add(9)<<endl;
+    add(5,4);
+    return 0;
+}
