447cb1b01f78fc435aee741740d91718d1ce2443
asifs-cse <asifs.cse@gmail.com>
2022-01-28 20:42:59 +0530

    addition two int and float using template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<class T> 
+T add(T a, T b){
+    return a+b;
+}
+
+int main(){
+    int a=2, b=3;
+    float m=3.5, n=4.6;
+    cout<<"Addition of a and b: "<<add(a,b)<<endl;
+    cout<<"Addition of m and n: "<<add(m,n)<<endl;
+
+    return 0;
+
+}
