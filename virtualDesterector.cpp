b221c6a4cea2f2b4a3caef931b7979824b3a0490
asifs-cse <asifs.cse@gmail.com>
2022-02-08 23:29:03 +0530

    constructor destrector create

--- virtualDesterector.cpp
+++ virtualDesterector.cpp
@@ -1,22 +1,29 @@
-#include<bits/stdc++.h>
+// A program with virtual destructor
+#include <iostream>
+
 using namespace std;
 
-class base{
-    public:
-        base(){cout<<"Construting base"<<endl;}
-        virtual ~base(){cout<<"Destrituting base\n";}
+class base {
+public:
+	base()	
+	{ cout << "Constructing base\n"; }
+	virtual ~base()
+	{ cout << "Destructing base\n"; }	
 };
 
-class derived: public base{
-    public:
-        derived(){cout<<"Constrting derived\n";}
-        virtual ~derived(){"Destrituting derived\n";}
+class derived : public base {
+public:
+	derived()	
+	{ cout << "Constructing derived\n"; }
+	virtual ~derived()
+	{ cout << "Destructing derived\n"; }
 };
 
-int main(){
-    derived *d = new derived();
-    base *b = d;
-    delete b;
+int main()
+{
+derived *d = new derived();
+base *b = d;
+delete b;
 
-    return 0;
+return 0;
 }
