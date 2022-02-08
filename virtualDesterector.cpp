6e192ca6db8dd577c8088757b7619c34b776c682
asifs-cse <asifs.cse@gmail.com>
2022-02-08 23:12:18 +0530

    create base class

--- virtualDesterector.cpp
+++ virtualDesterector.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class base{
+    public:
+        base(){cout<<"Construting base"<<endl;}
+        virtual ~base(){cout<<"Destrituting base\n";}
+};
+
 int main(){
     cout<<"Virtual destrector"<<endl;
     return 0;
